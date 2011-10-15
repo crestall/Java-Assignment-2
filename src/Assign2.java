7320fb82abbf1bc19717aeaff12306a17b5f89a8
Adrian Cowan (othrayte) <othrayte@gmail.com>
2011-10-16 00:54:25 +1100

    Added check to throw and error if no useable recipes are found

--- src/Assign2.java
+++ src/Assign2.java
@@ -11,13 +11,18 @@ public class Assign2 {
 
 		try {	
 			ArrayList<CakeRecipe> recipes = CakeRecipeUtil.parseDirectory(args[0]);
+			if (recipes.size() == 0)
+			{
+				//TODO: Create custom exception type
+				throw new Exception("No files found in directory: "+args[0]);
+			}
 			for (CakeRecipe recipe:recipes)
 			{
 				FileWriter file = new FileWriter(recipe.getName()+".txt");
 				file.write(recipe.output());
 				file.close();
 			}
-
+			
    			Gui theGui = new Gui();
    			theGui.launchFrame();
 		} catch (Exception e) {
